fd19fec793276e752534ba3bacb224c9302d8e0e
othieno <nicothieno@gmail.com>
2021-07-28 23:24:28 +0300

    structured,styled and made the page responsive

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 
 // user interface logic
 $(document).ready(function() {
@@ -19,4 +22,10 @@ $(document).ready(function() {
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
   });
+	$(".contact").last().click(function() {
+  $("#show-contact").show();
+  $("#show-contact h2").text(newContact.firstName);
+  $(".first-name").text(newContact.firstName);
+  $(".last-name").text(newContact.lastName);
+});
 });
